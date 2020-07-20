b19ba348fabae8656a5686a87064eca677a77f44
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2020-07-20 21:51:44 +0545

    Initial Commit

---
+++ index.php
@@ -0,0 +1,69 @@
+<?php session_start(); ?>
+<?php include('dbcon.php'); ?>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Blood Donor Management System</a>
+    </div>
+
+    <ul class="nav navbar-nav navbar-right">
+      <!-- <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li> -->
+      <li><a href="userlog/userlogin.php" ><span class="glyphicon glyphicon-log-in"></span> User Login</a></li>
+    </ul>
+  </div>
+</nav>
+
+
+<div class="form-wrapper">
+  
+  <form action="#" method="post">
+    <h3><center>Login Here</center></h3>
+	
+    <div class="form-item">
+		<input type="text" name="user" required="required" placeholder="Username" autofocus required></input>
+    </div>
+    
+    <div class="form-item">
+		<input type="password" name="pass" required="required" placeholder="Password" required></input>
+    </div>
+    
+    <div class="button-panel">
+		<input type="submit" class="button" title="Log In" name="login" value="Admin Login"></input>
+    </div>
+  </form>
+  <?php
+	if (isset($_POST['login']))
+		{
+			$username = mysqli_real_escape_string($con, $_POST['user']);
+			$password = mysqli_real_escape_string($con, $_POST['pass']);
+			
+			$query 		= mysqli_query($con, "SELECT * FROM admin WHERE  password='$password' and username='$username'");
+			$row		= mysqli_fetch_array($query);
+			$num_row 	= mysqli_num_rows($query);
+			
+			if ($num_row > 0) 
+				{			
+					$_SESSION['user_id']=$row['user_id'];
+					header('location:pages/index.php');
+					
+				}
+			else
+				{
+					echo 'Invalid Username and Password';
+				}
+		}
+  ?>
+  
+</div>
+
+</body>
+</html>
