6a6379523e9ca9008cc184c9bf5f3e6024d071ba
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2020-10-16 19:58:42 +0545

    changes in view campaign

--- pages/viewcampaign.php
+++ pages/viewcampaign.php
@@ -1,33 +1,6 @@
 <html>
 
 <head>
-<style>
-#donor {
-font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
-border-collapse: collapse;
-width: 80%;
-margin-top:100px;
-margin-left:80px;
-}
-
-#donor td, #customers th {
-border: 1px solid #ddd;
-padding: 8px;
-text-align:center;
-}
-
-#donor tr:nth-child(even){background-color: #f2f2f2;}
-
-#donor tr:hover {background-color: #ddd;}
-
-#donor th {
-padding-top: 12px;
-padding-bottom: 12px;
-text-align: center;
-background-color: #e7e7e7;
-color: black;
-}
-</style>
 
 <title>BDMS</title>
 
@@ -154,45 +127,71 @@ color: black;
             <!-- /.navbar-static-side -->
         </nav>
 
-
 <div id="page-wrapper">
+<div class="container-fluid">
 <div class="row">
-<div class="col-lg-12">
-                    <h1 class="page-header"><center>---CAMPAIGNS---</center></h1>
+<div class=".col-lg-12">
+               <h1 class="page-header">Campaign Details</h1>
                 </div>
-                
-<?php
+  </div>  
 
-include "dbconnect.php";
+				<div class="row">
+                        <div class=".col-lg-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    Total Records of available campaign
+                                </div>
+								
+								 <div class="panel-body">
+                                    <div class="table-responsive">
+									<table class="table table-striped table-bordered table-hover" id="dataTables-example">
+									
+									<?php
 
-$qry="select * from campaigndb";
-$result=mysqli_query($conn,$qry);
+						include "dbconnect.php";
+
+						$qry="select * from campaigndb";
+						$result=mysqli_query($conn,$qry);
 
 
-echo"<table border='1' id='donor'>
-<tr>
-    <th>Campaign Name</th>
-    <th>Organizer Name</th>
-    <th>Phone Number</th>
-    <th>Campaign Date</th>
-    <th>Description</th>
-</tr>";
+						echo"
+						<thead>
+												
+						<tr>
+						<th>Campaign Name</th>
+						<th>Organizer Name</th>
+						<th>Phone Number</th>
+						<th>Date of campaign</th>
+						<th>Description</th>
+					</tr>
+						</thead>";
 
-while($row=mysqli_fetch_array($result)){
-  echo"<tr>
-  <td>".$row['cname']."</td>
-  <td>".$row['oname']."</td>
-  <td>".$row['phn']."</td>
-  <td>".$row['cdate']."</td>
-  <td>".$row['descp']."</td>
+						while($row=mysqli_fetch_array($result)){
+						  echo"<tbody>
+						  <tr>
+						  <td>".$row['cname']."</td>
+						  <td>".$row['oname']."</td>
+						  <td>".$row['phn']."</td>
+						  <td>".$row['cdate']."</td>
+						  <td>".$row['descp']."</td>
 
-</tr>";
-}
+						</tr>
+						</tbody>";
+						}
 
-?>
+						?>
+						</table>
+									
+				</div>
+				</div>		
+		</div>
+		</div>	
+		</div>	
+		</div>
+		</div>
+
 </div>
-</div>
-</div>
+
 
   <!-- jQuery -->
   <script src="../vendor/jquery/jquery.min.js"></script>
