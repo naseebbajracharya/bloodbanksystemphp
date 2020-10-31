ccc17e41684a31b4cb6b4748edff9e938459dd5e
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2020-10-31 18:56:22 +0545

    view donor section removed from user's panel 2

--- userlog/userviewblood.php
+++ userlog/userviewblood.php
@@ -87,13 +87,9 @@ color: black;
                       
                             <!-- /.nav-second-level -->
                         </li>
-                       
-                        <li>
-                            <a href="userviewdonor.php"><i class="fa fa-edit fa-eye"></i> View Donor Details</a>
-                        </li>
                         
                         <li>
-                            <a href="userviewannouncements.php"><i class="fa fa-bullhorn"></i> View Announcements </a>
+                            <a href="userviewannouncement.php"><i class="fa fa-bullhorn"></i> View Announcements </a>
                      
                         </li>
 
@@ -109,50 +105,75 @@ color: black;
 
 
 <div id="page-wrapper">
+<div class="container-fluid">
 <div class="row">
-<div class="col-lg-12">
-                    <h1 class="page-header">BLOOD COLLECTION</h1>
+<div class=".col-lg-12">
+               <h1 class="page-header">Blood Collection</h1>
                 </div>
-                
-<?php
+  </div>  
 
-include "../pages/dbconnect.php";
+				<div class="row">
+                        <div class=".col-lg-12">
+                            <div class="panel panel-default">
+                                <div class="panel-heading">
+                                    Total Records of available bloods
+                                </div>
+								
+								 <div class="panel-body">
+                                    <div class="table-responsive">
+									<table class="table table-striped table-bordered table-hover" id="dataTables-example">
+									
+									<?php
 
-$qry="select * from blood";
-$result=mysqli_query($conn,$qry);
+						include "../pages/dbconnect.php";
+
+						$qry="select * from blood";
+						$result=mysqli_query($conn,$qry);
 
 
-echo"<table border='1' id='donor'>
-<tr>
-    <th>Blood Group</th>
-    <th>Full Name</th>
-    <th>Gender</th>
-    <th>D.O.B</th>
-    <th>Weight</th>
-    <th>Address</th>
-    <th>Contact</th>
-    <th>Blood Quantity</th>
-    <th>Collection Date</th>
-</tr>";
+						echo"
+						<thead>
+						<tr>
+							<th>Blood Group</th>
+							<th>Full Name</th>
+							<th>Gender</th>
+							<th>D.O.B</th>
+							<th>Weight</th>
+							<th>Address</th>
+							<th>Contact</th>
+							<th>Blood Quantity</th>
+							<th>Collection Date</th>
+						</tr>
+						</thead>";
 
-while($row=mysqli_fetch_array($result)){
-  echo"<tr>
-  <td>".$row['bloodgroup']."</td>
-  <td>".$row['name']."</td>
-  <td>".$row['gender']."</td>
-  <td>".$row['dob']."</td>
-  <td>".$row['weight']."</td>
-  <td>".$row['address']."</td>
-  <td>".$row['contact']."</td>
-  <td>".$row['bloodqty']."</td>
-  <td>".$row['collection']."</td>
+						while($row=mysqli_fetch_array($result)){
+						  echo"<tbody>
+						  <tr class='gradeA'>
+						  <td>".$row['bloodgroup']."</td>
+						  <td>".$row['name']."</td>
+						  <td>".$row['gender']."</td>
+						  <td>".$row['dob']."</td>
+						  <td>".$row['weight']."</td>
+						  <td>".$row['address']."</td>
+						  <td>".$row['contact']."</td>
+						  <td>".$row['bloodqty']."</td>
+						  <td>".$row['collection']."</td>
 
-</tr>";
-}
+						</tr>
+						<tbody>
+						";
+						}
 
-?>
-</div>
-</div>
+						?>
+						</table>
+									
+				</div>
+				</div>		
+		</div>
+		</div>	
+		</div>	
+		</div>
+		</div>
 </div>
 
   <!-- jQuery -->
