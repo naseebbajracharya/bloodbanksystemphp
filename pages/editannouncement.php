227ba5cca5c64ff24f84c170c90b007fe8e502a0
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2020-12-09 15:08:14 +0545

    missing fn added in edit announcement form - admin panel

--- pages/editannouncement.php
+++ pages/editannouncement.php
@@ -23,13 +23,6 @@
     <!-- Custom Fonts -->
     <link href="../vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
 </head>
 
 <body>
@@ -48,6 +41,25 @@
                 <a class="navbar-brand" href="index.php">BLOOD DONORS MANAGEMENT SYSTEM</a>
             </div>
             <!-- /.navbar-header -->
+			
+			<ul class="nav navbar-top-links navbar-right">
+               
+             
+                <!-- /.dropdown -->
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <!-- <li class="divider"></li> -->
+                        <li><a href="../logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-user -->
+                </li>
+               
+                <!-- /.dropdown -->
+            </ul>
 
             <!-- /.navbar-top-links -->
 
