9ddded1c641570b5f0ca9c8d3a59879e44240bea
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2020-12-14 13:45:49 +0545

    minor changes in title; title misplaced

--- pages/editform.php
+++ pages/editform.php
@@ -159,7 +159,7 @@
         <div id="page-wrapper">
             <div class="row">
                 <div class="col-lg-12">
-                    <h1 class="page-header">Edit Blood Details</h1>
+                    <h1 class="page-header">Edit Donor's Detail</h1>
                 </div>
                 <!-- /.col-lg-12 -->
             </div>
