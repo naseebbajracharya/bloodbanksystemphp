5f583f363a07a74780a5bfc6ebbce7a2e4ea4e62
naseebbajracharya <34349150+naseebbajracharya@users.noreply.github.com>
2020-12-19 19:55:40 +0545

    missing fn added in edit.php file; admin panel

--- pages/edit.php
+++ pages/edit.php
@@ -47,9 +47,28 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">SB Admin v2.0</a>
+                <a class="navbar-brand" href="index.html">BBMS</a>
             </div>
             <!-- /.navbar-header -->
+			
+			<ul class="nav navbar-top-links navbar-right">
+               
+             
+                <!-- /.dropdown -->
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                        <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
+                    </a>
+                    <ul class="dropdown-menu dropdown-user">
+                        <!-- <li class="divider"></li> -->
+                        <li><a href="../logout.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+                        </li>
+                    </ul>
+                    <!-- /.dropdown-user -->
+                </li>
+               
+                <!-- /.dropdown -->
+            </ul>
 
             <!-- /.navbar-top-links -->
 
